8ae9cd2e1e5f35492e8d91e01f8ebedd408438da
Oleksandr Korniienko <oleksanderkorn@gmail.com>
2021-11-03 19:33:16 +0100

    Near Certified Developer II React App.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "regenerator-runtime/runtime";
+import React from "react";
+import PropTypes from "prop-types";
+import YatziGame from "./Game";
 
-function App() {
+const App = ({ contract, currentUser, nearConfig, wallet }) => {
+  const signIn = () => {
+    wallet.requestSignIn(nearConfig.contractName, "NEAR Yatzi App");
+  };
+
+  const signOut = () => {
+    wallet.signOut();
+    window.location.replace(window.location.origin + window.location.pathname);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div className="flex-shrink-0">
+          <img
+            className="h-12 w-24"
+            src="https://docs.near.org/img/near_logo.svg"
+            alt="ChitChat Logo"
+          />
+          {currentUser ? (
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={signOut}
+            >
+              Log out
+            </button>
+          ) : (
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={signIn}
+            >
+              Log in
+            </button>
+          )}
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">
+            NCD II: Yatzy Game
+          </div>
+          {currentUser ? (
+            <div>
+              <p className="text-gray-500">
+                Account ID: {currentUser.accountId}{" "}
+              </p>
+            </div>
+          ) : (
+            <div>
+              <p className="text-gray-500">Sign In To Use The App</p>
+            </div>
+          )}
+        </div>
+      </div>
+      {currentUser && <YatziGame contract={contract} />}
+    </>
   );
-}
+};
+
+App.propTypes = {
+  contract: PropTypes.shape({
+    current_points: PropTypes.func.isRequired,
+    current_turn: PropTypes.func.isRequired,
+    start_round: PropTypes.func.isRequired,
+    end_round: PropTypes.func.isRequired,
+    reset_game: PropTypes.func.isRequired,
+  }).isRequired,
+  currentUser: PropTypes.shape({
+    accountId: PropTypes.string.isRequired,
+    balance: PropTypes.string.isRequired,
+  }),
+  nearConfig: PropTypes.shape({
+    contractName: PropTypes.string.isRequired,
+  }).isRequired,
+  wallet: PropTypes.shape({
+    requestSignIn: PropTypes.func.isRequired,
+    signOut: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default App;
---
+++ src/Game.jsx
@@ -0,0 +1,154 @@
+import { useEffect, useState } from "react";
+import ErrorPopup from "./ErrorPupop";
+
+const YatziGame = ({ contract }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [score, setScore] = useState("");
+  const [dice, setDice] = useState("");
+  const [error, setError] = useState("");
+  const [howToPlay, setHowToPlay] = useState("");
+  const [currentTurn, setCurrentTurn] = useState("");
+
+  useEffect(() => {
+    contract.how_to_play().then((text) => setHowToPlay(text));
+    contract.current_points().then((points) => setScore(points));
+    contract.current_turn().then((turn) => setCurrentTurn(turn));
+  });
+
+  const resetGame = () => {
+    setIsLoading(true);
+    contract.reset_game().then(
+      () => {
+        setIsLoading(false);
+      },
+      (err) => {
+        // alert(err);
+        setError(`${err}`);
+        setIsLoading(false);
+      }
+    );
+  };
+
+  const isValidDice = () =>
+    dice !== "" &&
+    (dice === "1" ||
+      dice === "1" ||
+      dice === "2" ||
+      dice === "3" ||
+      dice === "4" ||
+      dice === "5" ||
+      dice === "6");
+
+  const chooseCombination = () => {
+    setIsLoading(true);
+    if (isValidDice()) {
+      contract.end_round({ combination: parseInt(dice) }).then(
+        () => {
+          setIsLoading(false);
+          setDice("");
+        },
+        (err) => {
+          // alert(err);
+          setError(`${err}`);
+          setIsLoading(false);
+        }
+      );
+    }
+  };
+  const rollTheDice = () => {
+    setIsLoading(true);
+    contract.start_round().then(
+      () => {
+        setIsLoading(false);
+      },
+      (err) => {
+        // alert(err);
+        setError(`${err}`);
+        setIsLoading(false);
+      }
+    );
+  };
+
+  return (
+    <div className="md:container md:mx-auto">
+      {isLoading ? (
+        <div className="mt-4 mx-auto loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-20 w-20"></div>
+      ) : (
+        <div className="mt-4 h-20 w-20"></div>
+      )}
+      <div className="p-8">
+        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+          How to play
+        </div>
+        <p className="mt-2 text-gray-500">{howToPlay}</p>
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <div className="p-8">
+            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+              Current turn
+            </div>
+            <p className="mt-2 text-gray-500">
+              {currentTurn === ""
+                ? "Click roll the dice button"
+                : currentTurn.split("").join(" ")}
+            </p>
+          </div>
+        </div>
+        <div>
+          <div className="p-8">
+            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+              Current score
+            </div>
+            <p className="mt-2 text-gray-500">
+              {score ? score.split(".")[0] : "No score available"}
+            </p>
+            <p className="mt-2 text-gray-500">
+              {score ? score.replace(`${score.split(".")[0]}.`, "") : ""}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 gap-4">
+        <div>
+          <div className="p-8">
+            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+              Game Actions
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={rollTheDice}
+              >
+                Roll the dice
+              </button>
+              <button
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                onClick={resetGame}
+              >
+                Reset game
+              </button>
+              <button
+                disabled={!isValidDice()}
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                onClick={chooseCombination}
+              >
+                Choose Combination
+              </button>
+              <input
+                type="text"
+                className="border-2 p-1 border-green-500 rounded"
+                value={dice}
+                placeholder="Enter Dice from 1 to 6"
+                onChange={(event) => setDice(event.target.value)}
+              />
+            </div>
+            <ErrorPopup error={error} callback={() => setError("")} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default YatziGame;
